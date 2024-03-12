c4a2a2637fd9080722d70fd024cc6792d1928976
aytheeh2 <aytheeh@gmail.com>
2024-03-12 12:32:56 +0530

    before implimenting order id

--- cart/models.py
+++ cart/models.py
@@ -30,6 +30,18 @@ class Order_Table(models.Model):
     no_of_items_ordered = models.IntegerField()
     address = models.TextField()
     phone = models.CharField(max_length=10)
+    total_price = models.FloatField(null=False)
+    payment_id = models.IntegerField(null=True)
 
     def __str__(self):
         return f'{self.Products.name}'
+
+
+class order_id_table(models.Model):
+    products = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order_Table, on_delete=models.CASCADE)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.order
